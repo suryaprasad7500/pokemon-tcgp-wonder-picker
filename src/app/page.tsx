f56b6d36078084b7c7910a4a8616bbbafbd3a991
Surya Malempati <surya.prasad.7500@gmail.com>
2025-02-16 18:27:09 -0600

    Issue #5 adding function to pick card and change the image on the picked card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
+'use client';
 import Grid from "@mui/material/Grid2"
 import styles from "./page.module.css";
 import PokemonCard from "./ui/pokemon-card";
 import { Button } from "@mui/material";
+import randomPicker from "./lib/random-picker"
+import { useState } from "react";
 
 // importing icons
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Home() {
+  const [pickedCard, setPickedCard] = useState(0);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <PokemonCard />
+        <PokemonCard 
+          pickedCard = {pickedCard}
+        />
         <Grid container justifyContent="center" alignItems="center">
           <Grid size = {6}>
-            <Button variant="contained" fullWidth>
+            <Button variant="contained" fullWidth onClick={() => setPickedCard(randomPicker())}>
               Pick a card
+            </Button>
+            <Button variant="outlined" fullWidth onClick={() => setPickedCard(0)} disabled = {pickedCard === 0}>
+              Reset
             </Button>
           </Grid>
         </Grid>
--- src/app/ui/pokemon-card.tsx
+++ src/app/ui/pokemon-card.tsx
@@ -3,10 +3,10 @@ import cardBack from "../../../public/tcg-card-back.jpg"
 import cardFront from "../../../public/tcg-card-front.png"
 import Image from "next/image"
 
-let currentFace = cardBack;
 
-export default function PokemonCard() {
+export default function PokemonCard({ pickedCard }) {
     const cards = [1, 2, 3, 4, 5]
+    console.debug(`Picked card is ${pickedCard}`)
     return (
         <div>
         <Grid container spacing = {2} justifyContent="center" alignItems="center" >
@@ -15,9 +15,11 @@ export default function PokemonCard() {
                     <Grid size={4} key={number}>
                         <Image
                             key = {number}
-                            src = {cardBack}
+                            src = {
+                                pickedCard === number ? cardFront : cardBack
+                            }
                             alt = "Pokemon Card Back"
-                            style={{ maxWidth: "100%", height: "auto" }}
+                            style = {{ maxWidth: "100%", height: "auto" }}
                         />
                     </Grid>
                 )
